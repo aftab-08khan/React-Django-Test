8239b8a6aa572ddbbb4bc0e597dea74cbb94e1f5
aftab-08khan <akkhanaftab08@gmail.com>
2026-06-22 11:41:21 +0400

    Updated - Updated the Code With Filtering Logic

--- backend-drf/books/views.py
+++ backend-drf/books/views.py
@@ -1,10 +1,14 @@
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.filters import SearchFilter
 from rest_framework import permissions
+from django_filters.rest_framework import DjangoFilterBackend
 class BookListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Book.objects.all()
+    queryset = Book.objects.all().select_related('author')
 
     serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]    
+    search_fields =  ['title','author']
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     
